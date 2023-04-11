75d596d16d67c4c2154a9d7e28fcc85b3b63b1e0
fayung <124017942+fayung@users.noreply.github.com>
2023-04-11 11:42:29 +0700

    Add files via upload

---
+++ asis/assets/data.js
@@ -0,0 +1,27 @@
+var Items = [
+    {
+      name: "Women's Long-Sleeve Crop Top",
+      sku: 1,
+      price: "$50",
+      category: "top",
+      status: "Sale 50%",
+      image: "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/176f3892-1351-48d0-a160-45998177f50a/sportswear-womens-long-sleeve-crop-top-86kP9n.png"
+    },
+    {
+        name: "Nike Sportswear Phoenix Fleece",
+        sku: 2,
+        price: "$70",
+        category: "bottom",
+        status: "Best Seller",
+        image: "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/d4c3ae75-2ca0-452e-9ad2-82d89d11d4ed/sportswear-phoenix-fleece-womens-high-waisted-oversized-sweatpants-sftDBs.png"
+      },
+      {
+        name: "Nike Sportswear Phoenix Fleece",
+        sku: 2,
+        price: "$70",
+        category: "bottom",
+        status: "Best Seller",
+        image: "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/d4c3ae75-2ca0-452e-9ad2-82d89d11d4ed/sportswear-phoenix-fleece-womens-high-waisted-oversized-sweatpants-sftDBs.png"
+
+      }
+    ]
